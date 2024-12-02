c6a514153f0349b13bc68db21feb3df6bf0bc1ac
valentin <germanov.ve@gmail.com>
2024-12-02 11:39:52 +0300

    HW1 is completed

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"github.com/germanov-v/go_h_24/hw01_hello_otus/stringutil"
+	"golang.org/x/example/hello/reverse"
+	"log"
+)
+
 func main() {
 	// Place your code here.
+	strStart := "Hello, OTUS!"
+	strResultExReverse := reverse.String(strStart)
+	strResultStrutil := stringutil.Reverse(strStart)
+
+	if strResultExReverse != strResultStrutil {
+		log.Fatalf("String is not equal: %s != %s\n", strResultExReverse, strResultStrutil)
+	} else {
+		fmt.Println(strResultExReverse)
+	}
+
 }
