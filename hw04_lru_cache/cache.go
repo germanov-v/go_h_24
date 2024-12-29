a1315b27f7e0df6096d335a9f576a623a64b23cf
valentin <germanov.ve@gmail.com>
2024-12-29 11:01:36 +0300

    HW4 pass exist cache tests

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -9,8 +9,7 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
+	//Cache    // Remove me after realization.
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
@@ -25,23 +24,28 @@ func NewCache(capacity int) Cache {
 }
 
 func (lc *lruCache) Set(key Key, value interface{}) bool {
-
+	existed := false
 	if item, ok := lc.items[key]; ok {
+		item.Value = value
 		lc.queue.MoveToFront(item)
+		existed = true
 	} else {
 		if lc.capacity < lc.queue.Len() {
 			lc.queue.Remove(lc.queue.Back())
 		}
-		newItem := lc.queue.PushFront(item)
+		newItem := lc.queue.PushFront(value)
 		lc.items[key] = newItem
 	}
 
-	return true
+	return existed
 }
 
 func (lc *lruCache) Get(key Key) (interface{}, bool) {
-	item, ok := lc.items[key]
-	return item.Value, ok
+	if item, ok := lc.items[key]; !ok {
+		return nil, ok
+	} else {
+		return item.Value, ok
+	}
 }
 
 func (lc *lruCache) Clear() {
