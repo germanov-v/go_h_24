da74807601bacc5b83a71d25c1d305e47aabe6dc
valentin <germanov.ve@gmail.com>
2024-12-29 09:58:45 +0300

    HW4 added draft cache.go (initial)

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -26,7 +26,22 @@ func NewCache(capacity int) Cache {
 
 func (lc *lruCache) Set(key Key, value interface{}) bool {
 
+	if item, ok := lc.items[key]; ok {
+		lc.queue.MoveToFront(item)
+	} else {
+		if lc.capacity < lc.queue.Len() {
+			lc.queue.Remove(lc.queue.Back())
+		}
+		newItem := lc.queue.PushFront(item)
+		lc.items[key] = newItem
+	}
+
 	return true
+}
+
+func (lc *lruCache) Get(key Key) (interface{}, bool) {
+	item, ok := lc.items[key]
+	return item.Value, ok
 }
 
 func (lc *lruCache) Clear() {
