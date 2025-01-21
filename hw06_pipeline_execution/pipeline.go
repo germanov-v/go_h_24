85caae7b68301441eb7e88ae60c39dd22be7e71c
valentin <germanov.ve@gmail.com>
2025-01-21 09:49:51 +0300

    HW5 added draft with failed test

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,5 +1,7 @@
 package hw06pipelineexecution
 
+import "sync"
+
 type (
 	In  = <-chan interface{}
 	Out = In
@@ -8,20 +10,53 @@ type (
 
 type Stage func(in In) (out Out)
 
+// after race detected
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
+
+	var wg sync.WaitGroup
+
+	shadowStream := make(Bi)
+	out := make(Bi)
+	shadowProc := func(ch <-chan interface{}) {
+		defer wg.Done()
+		for i := range ch {
+			select {
+			case <-done:
+				return
+			case shadowStream <- i: // продолжаем работу
+				out <- shadowStream
+			}
+		}
+	}
+
+	for _, stage := range stages {
+		wg.Add(1)
+		shadowProc(stage(in))
+	}
+
+	go func() {
+		wg.Wait()
+		close(shadowStream)
+	}()
+
+	return out
+}
+
+// alarm: race detecteted
+func ExecutePipelineV3(in In, done In, stages ...Stage) Out {
 	out := make(Bi) //make(chan<- interface{})
+
 	go func() {
 		defer close(out)
 		current := in
 		for _, stage := range stages {
 			current = stage(current)
 		}
+
 		for {
 			select {
 			case <-done:
 				for range current {
-
-					_ = <-current
 				}
 				return
 			case val, ok := <-current:
@@ -29,12 +64,9 @@ func ExecutePipeline(in In, done In, stages ...Stage) Out {
 					return
 				}
 				out <- val
-				//default:
-				// ?????
 			}
 		}
 
 	}()
-
 	return out
 }
