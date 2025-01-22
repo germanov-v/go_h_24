c096b58e9b86568dac426868221f974b4b26188f
valentin <germanov.ve@gmail.com>
2025-01-22 10:44:54 +0300

    HW5 added draft ExecutePipeline

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -10,28 +10,58 @@ type (
 
 type Stage func(in In) (out Out)
 
-// after race detected
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
 
 	var wg sync.WaitGroup
 
 	shadowStream := make(Bi)
-	out := make(Bi)
-	shadowProc := func(ch <-chan interface{}) {
+	out := in
+	shadowProc := func(ch <-chan interface{}, stage Stage) {
+		defer wg.Done()
+		for i := range ch {
+			select {
+			case <-done:
+				return
+			case shadowStream <- i:
+				out = stage(shadowStream)
+			}
+		}
+	}
+	go shadowProc(done, nil)
+	for _, stage := range stages {
+		wg.Add(1)
+		go shadowProc(out, stage)
+	}
+
+	go func() {
+		wg.Wait()
+		close(shadowStream)
+	}()
+
+	return out
+}
+
+func ExecutePipelineV4(in In, done In, stages ...Stage) Out {
+
+	var wg sync.WaitGroup
+
+	shadowStream := make(Bi)
+	out := make(In)
+	shadowProc := func(ch <-chan interface{}, stage Stage) {
 		defer wg.Done()
 		for i := range ch {
 			select {
 			case <-done:
 				return
 			case shadowStream <- i: // продолжаем работу
-				out <- shadowStream
+				out = stage(shadowStream)
 			}
 		}
 	}
 
 	for _, stage := range stages {
 		wg.Add(1)
-		shadowProc(stage(in))
+		shadowProc(in, stage)
 	}
 
 	go func() {
