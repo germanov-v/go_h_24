52fc00433f8db01b94485a67886c8a8d26ac71ae
Valentin <55596166+germanov-v@users.noreply.github.com>
2025-01-30 20:18:18 +0300

    hw05_parallel_execution (#5)
    * HW5 draft (contain: deadlock, race)
    
    * HW5 draft (without: deadlock, race)
    
    * HW5 draft runCh
    
    * HW5 draft runCh
    
    * HW5 added draft tests
    
    * HW5 added draft tests
    
    * HW5 added draft tests RunChNonBuffer
    
    * HW5 added draft tests RunChNonBuffer
    
    * HW5 added fixes RunChNonBuffer

--- hw05_parallel_execution/go.mod
+++ hw05_parallel_execution/go.mod
@@ -1,4 +1,4 @@
-module github.com/fixme_my_friend/hw05_parallel_execution
+module github.com/germanov-v/go_h_24/hw05_parallel_execution
 
 go 1.22
 
--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,6 +2,7 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
@@ -9,7 +10,161 @@ var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
-func Run(tasks []Task, n, m int) error {
+func Run(tasks []Task, countWorkers, countErrors int) error {
 	// Place your code here.
+
+	var wg sync.WaitGroup
+	var lock sync.Mutex
+	needStopByLimitErrors := false
+	currentCountErrors := 0
+	for i := 0; i < countWorkers; i++ {
+		lock.Lock()
+		if needStopByLimitErrors {
+			lock.Unlock()
+			break
+		}
+		lock.Unlock()
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			//defer lock.Unlock()
+
+			for {
+
+				lock.Lock()
+				if needStopByLimitErrors || len(tasks) == 0 {
+					lock.Unlock()
+					break
+				}
+				task := tasks[0]
+				tasks = tasks[1:]
+				lock.Unlock()
+
+				err := task()
+				if err != nil {
+					lock.Lock()
+					currentCountErrors++
+					if countErrors <= currentCountErrors {
+						needStopByLimitErrors = true
+					}
+					lock.Unlock()
+				}
+
+			}
+
+		}()
+	}
+
+	wg.Wait()
+
+	if needStopByLimitErrors {
+		return ErrErrorsLimitExceeded
+	}
+
+	return nil
+}
+
+func RunCh(tasks []Task, countWorkers, countErrors int) error {
+	// Place your code here.
+
+	var wg sync.WaitGroup
+	var lock sync.Mutex
+	needStopByLimitErrors := false
+	currentCountErrors := 0
+
+	ch := make(chan Task, countWorkers)
+
+	go func() {
+		//	for i := 0; i < len(tasks); i++ {
+		for _, item := range tasks {
+			ch <- item
+		}
+		close(ch)
+	}()
+
+	for i := 0; i < countWorkers; i++ {
+		lock.Lock()
+		if needStopByLimitErrors {
+			lock.Unlock()
+			break
+		}
+		lock.Unlock()
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			//defer lock.Unlock()
+
+			for t := range ch {
+
+				lock.Lock()
+				if needStopByLimitErrors || len(tasks) == 0 {
+					lock.Unlock()
+					break
+				}
+				lock.Unlock()
+
+				if err := t(); err != nil {
+					lock.Lock()
+					currentCountErrors++
+					if countErrors <= currentCountErrors {
+						needStopByLimitErrors = true
+					}
+					lock.Unlock()
+				}
+
+			}
+
+		}()
+	}
+
+	wg.Wait()
+
+	if needStopByLimitErrors {
+		return ErrErrorsLimitExceeded
+	}
+
+	return nil
+}
+
+func RunChNonBuffer(tasks []Task, countWorkers, countErrors int) error {
+	var wg sync.WaitGroup
+	var lock sync.Mutex
+	currentCountErrors := 0
+
+	ch := make(chan Task)
+
+	for i := 0; i < countWorkers; i++ {
+
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for t := range ch {
+				if err := t(); err != nil {
+					lock.Lock()
+					currentCountErrors++
+					lock.Unlock()
+				}
+
+			}
+
+		}()
+	}
+	for _, item := range tasks {
+		lock.Lock()
+		if countErrors <= currentCountErrors {
+			lock.Unlock()
+			break
+		}
+		lock.Unlock()
+		ch <- item
+	}
+	close(ch)
+
+	wg.Wait()
+
+	if countErrors <= currentCountErrors {
+		return ErrErrorsLimitExceeded
+	}
+
 	return nil
 }
