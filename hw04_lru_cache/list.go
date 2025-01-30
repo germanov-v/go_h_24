f1126be210ab924655feb7814406d416cf379894
Valentin <55596166+germanov-v@users.noreply.github.com>
2025-01-30 20:18:01 +0300

    Hw04 lru cache (#4)
    * HW4 added draft
    
    * HW4 added draft
    
    * HW4 added draft
    
    * HW4 added draft
    
    * HW4 added draft cache.go (initial)
    
    * HW4 added draft cache.go (initial)
    
    * HW4 pass exist list tests
    
    * HW4 pass exist cache tests
    
    * HW4 pass exist cache tests + added new tests: substitute by time getting, "substitute by capacity, clear
    
    * HW4 added new list test TestListBasicOperations + fix bugs
    
    * HW4 .golangci.yaml
    
    * HW4 added fixes
    
    * HW4 added fixes

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -1,5 +1,9 @@
 package hw04lrucache
 
+func NewList() List {
+	return new(list)
+}
+
 type List interface {
 	Len() int
 	Front() *ListItem
@@ -17,10 +21,99 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
+	//List // Remove me after realization.
 	// Place your code here.
+
+	start  *ListItem
+	end    *ListItem
+	lenght int
+	//items     []ListItem        // extra
+	//positions map[*ListItem]int // extra
 }
 
-func NewList() List {
-	return new(list)
+func (l *list) Len() int {
+	return l.lenght
+}
+
+func (l *list) Front() *ListItem {
+	return l.start
+}
+
+func (l *list) Back() *ListItem {
+	return l.end
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	item := ListItem{Value: v}
+	if l.start != nil {
+		item.Next = l.start
+		l.start.Prev = &item
+	} else {
+		l.end = &item
+	}
+	l.start = &item
+	l.lenght++
+	return &item
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	item := ListItem{Value: v}
+	if l.end != nil {
+		item.Prev = l.end
+		l.end.Next = &item
+	} else {
+		l.start = &item
+	}
+
+	l.end = &item
+	l.lenght++
+	return &item
+}
+
+// TODO: has param 'i' actual pointers ? => O(1) without loop
+func (l *list) Remove(i *ListItem) {
+
+	if i == nil {
+		return
+	}
+
+	if i.Prev != nil && i.Next != nil {
+		i.Prev.Next = i.Next
+		i.Next.Prev = i.Prev
+		//i.Prev = nil
+	} else if i.Prev != nil {
+		i.Prev.Next = nil
+		l.end = i.Prev
+	} else if i.Next != nil { // i.Next != nil
+		i.Next.Prev = nil
+		l.start = i.Next
+	} else {
+		l.start = nil
+		l.end = nil
+	}
+	i.Next = nil
+	i.Prev = nil
+	l.lenght--
+}
+
+func (l *list) MoveToFront(i *ListItem) {
+
+	if l.start == i && i.Prev != nil {
+		i.Prev = nil
+		return
+	} else if l.start == i {
+		return
+	}
+
+	if i.Prev != nil && i.Next != nil {
+		i.Prev.Next = i.Next
+		i.Next.Prev = i.Prev
+	} else { //  i.Prev != nil
+		i.Prev.Next = nil
+		l.end = i.Prev
+	}
+	i.Next = l.start
+	l.start.Prev = i
+	i.Prev = nil
+	l.start = i
 }
