ddc907b66b83f820480a302b368b4fbc8c76715e
valentin <germanov.ve@gmail.com>
2025-01-30 14:49:05 +0300

    HW6 added draft SetProgressBar

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
@@ -11,5 +16,42 @@ var (
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
 	// Place your code here.
+
+	src, err := os.Open(fromPath)
+
+	if err != nil {
+		// оборачиваем исходную ошибку. пример:  *os.PathError,
+		// ErrUnsupportedFile
+		return fmt.Errorf("failed open file %w", err)
+	}
+	defer func(src *os.File) {
+		err := src.Close()
+		if err != nil {
+			// просто выводим в консоль ошибку, для диагностики
+			fmt.Printf("failed close file %s", err)
+		}
+	}(src)
+
+	//io.LimitedReader(src)
+
+	//reader := SetProgressBar(limit, src)
+
+	//	_, err = io.Copy(dst)
+
 	return nil
+}
+
+// TODO: начать с теста прогрессбара.
+// https://github.com/cheggaaa/pb
+// из примера: 37158 / 100000 [---------------->_______________________________] 37.16% 916 p/s
+func SetProgressBar(limit int64, reader io.Reader) *pb.Reader {
+	bar := pb.Full.Start64(limit)
+
+	// bar will format numbers as bytes (B, KiB, MiB, etc)
+	bar.Set(pb.Bytes, true)
+
+	//defer bar.Finish() // просто закрываем
+
+	proxyReader := bar.NewProxyReader(io.LimitReader(reader, limit))
+	return proxyReader
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,31 @@
 package main
 
-import "testing"
+import (
+	"bytes"
+	"github.com/stretchr/testify/assert"
+	"io"
+	"testing"
+)
 
 func TestCopy(t *testing.T) {
 	// Place your code here.
+}
+
+func TestSetProgressBar(t *testing.T) {
+	limit := int64(50)
+	data := bytes.Repeat([]byte{1}, int(limit))
+	reader := bytes.NewReader(data)
+
+	progressReader := SetProgressBar(limit, reader)
+
+	// Читаем данные из прогресс-бара
+	readData, err := io.ReadAll(progressReader)
+	//progressReader.Finish()
+	//time.After(5 * time.Millisecond)
+
+	if err != nil {
+		t.Fatalf("Error reading: %v", err)
+	}
+
+	assert.Equal(t, data, readData, "Data must be equak")
 }
