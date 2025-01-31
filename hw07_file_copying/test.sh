4984fe7585d9f18bb801cebe422d78814d48ba73
valentin <germanov.ve@gmail.com>
2025-01-31 14:32:46 +0300

    HW7 added tests

--- hw07_file_copying/test.sh
+++ hw07_file_copying/test.sh
@@ -5,19 +5,19 @@ go build -o go-cp
 
 ./go-cp -from testdata/input.txt -to out.txt
 cmp out.txt testdata/out_offset0_limit0.txt
-
+rm -f out.txt
 ./go-cp -from testdata/input.txt -to out.txt -limit 10
 cmp out.txt testdata/out_offset0_limit10.txt
-
+rm -f out.txt
 ./go-cp -from testdata/input.txt -to out.txt -limit 1000
 cmp out.txt testdata/out_offset0_limit1000.txt
-
+rm -f out.txt
 ./go-cp -from testdata/input.txt -to out.txt -limit 10000
 cmp out.txt testdata/out_offset0_limit10000.txt
-
+rm -f out.txt
 ./go-cp -from testdata/input.txt -to out.txt -offset 100 -limit 1000
 cmp out.txt testdata/out_offset100_limit1000.txt
-
+rm -f out.txt
 ./go-cp -from testdata/input.txt -to out.txt -offset 6000 -limit 1000
 cmp out.txt testdata/out_offset6000_limit1000.txt
 
