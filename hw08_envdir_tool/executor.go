cd6746f6cfa57ad19090fde9241d0ea30e7a4182
valentin <germanov.ve@gmail.com>
2025-02-04 21:02:41 +0300

    HW8 draft

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -3,5 +3,12 @@ package main
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
 	// Place your code here.
+
+	// 1.
+
+	// 2.
+
+	// 3.
+
 	return
 }
