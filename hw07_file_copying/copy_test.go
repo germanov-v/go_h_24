96eff55c6542aa1ebc000e20c95fb911b5759466
Valentin <55596166+germanov-v@users.noreply.github.com>
2025-02-04 21:03:09 +0300

    hw07_file_copying (#7)
    * HW1 is completed
    
    * fixes bugs stringutil
    
    * HW1 is completed (added go.sum)
    
    * HW6 added draft SetProgressBar
    
    * HW7 success executing main.go without bar
    
    * HW7 success executing main.go without bar
    
    * HW2 draft (#2)
    
    * HW2 draft
    
    * HW2 draft (fixed several bugs)
    
    * HW2 draft (fixed several bugs)
    
    * HW2 draft (fixed several bugs)
    
    * HW2 draft (added tests)
    
    * HW2 added fixes to go.mod
    
    * Hw03 frequency analysis (#3)
    
    * HW3 added draft
    
    * HW3 added draft
    
    * HW3 added draft
    
    * Hw04 lru cache (#4)
    
    * HW4 added draft
    
    * HW4 added draft
    
    * HW4 added draft
    
    * HW4 added draft
    
    * HW4 added draft cache.go (initial)
    
    * HW4 added draft cache.go (initial)
    
    * HW4 pass exist list tests
    
    * HW4 pass exist cache tests
    
    * HW4 pass exist cache tests + added new tests: substitute by time getting, "substitute by capacity, clear
    
    * HW4 added new list test TestListBasicOperations + fix bugs
    
    * HW4 .golangci.yaml
    
    * HW4 added fixes
    
    * HW4 added fixes
    
    * hw05_parallel_execution (#5)
    
    * HW5 draft (contain: deadlock, race)
    
    * HW5 draft (without: deadlock, race)
    
    * HW5 draft runCh
    
    * HW5 draft runCh
    
    * HW5 added draft tests
    
    * HW5 added draft tests
    
    * HW5 added draft tests RunChNonBuffer
    
    * HW5 added draft tests RunChNonBuffer
    
    * HW5 added fixes RunChNonBuffer
    
    * hw06_pipeline_execution (#6)
    
    * HW5 added bug draft
    
    * HW5 added bug draft
    
    * HW5 added draft with failed test
    
    * HW5 added draft with failed test
    
    * HW5 added draft with failed test
    
    * HW5 added draft ExecutePipeline
    
    * HW5 added draft ExecutePipeline
    
    * HW6 added draft ExecutePipeline
    
    * HW6 added draft ExecutePipeline
    
    * HW6 added draft ExecutePipeline
    
    * HW6 added draft ExecutePipeline
    
    * HW6 added draft SetProgressBar
    
    * HW7 success executing main.go without bar
    
    * HW7 success executing main.go without bar
    
    * HW7 added handle file path equal
    
    * HW7 added CopyByPartial with render progress bar
    
    * HW7 added handle errors exist destination
    
    * HW7 added handle errors exist destination
    
    * HW7 added handle errors exist destination
    
    * HW7 added tests
    
    * HW7 added tests
    
    * HW7 added tests

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,87 @@
 package main
 
-import "testing"
+import (
+	"bytes"
+	"github.com/stretchr/testify/assert"
+	"io"
+	"os"
+	"testing"
+	"time"
+)
 
 func TestCopy(t *testing.T) {
+	cleanDirTmp(t)
 	// Place your code here.
+	t.Run("with limit and offset", func(t *testing.T) {
+		err := Copy("testdata/input.txt", "tmp/1.txt", 0, 10)
+		assert.NoError(t, err)
+
+		_, err = os.Stat("tmp/1.txt")
+		assert.NoError(t, err)
+	})
+
+	t.Run("destination file exists", func(t *testing.T) {
+		_, _ = os.Create("tmp/2.txt")
+		err := Copy("testdata/input.txt", "tmp/2.txt", 0, 0)
+		assert.ErrorIs(t, err, ErrDestinationExistsFile)
+	})
+
+	t.Run("null source", func(t *testing.T) {
+		err := Copy("testdata/null.txt", "tmp/3.txt", 0, 0)
+		assert.ErrorIs(t, err, ErrOpenFile)
+	})
+
+	t.Run("/dev/null", func(t *testing.T) {
+		err := Copy("/dev/null", "tmp/4.txt", 0, 0)
+		assert.ErrorIs(t, err, ErrUnsupportedFile)
+	})
+
+	t.Run("error  offset", func(t *testing.T) {
+		err := Copy("testdata/input.txt", "tmp/5.txt", 100000, 0)
+		assert.ErrorIs(t, err, ErrOffsetExceedsFileSize)
+	})
+
+	t.Run("without offset", func(t *testing.T) {
+		err := Copy("testdata/input.txt", "tmp/6.txt", 0, 10000)
+		assert.NoError(t, err)
+
+		_, err = os.Stat("tmp/6.txt")
+		assert.NoError(t, err)
+	})
+
+	t.Run("with offset", func(t *testing.T) {
+		err := Copy("testdata/input.txt", "tmp/7.txt", 100, 0)
+		assert.NoError(t, err)
+
+		_, err = os.Stat("tmp/7.txt")
+		assert.NoError(t, err)
+	})
+}
+
+// calling: go test -v -run ^TestSetProgressBar$
+func TestSetProgressBar(t *testing.T) {
+	limit := int64(50)
+	data := bytes.Repeat([]byte{1}, int(limit))
+	reader := bytes.NewReader(data)
+
+	progressReader, bar := SetProgressBar(limit, reader)
+
+	bar.Finish()
+	readData, err := io.ReadAll(progressReader)
+	//progressReader.Finish()
+	time.After(5 * time.Millisecond)
+
+	if err != nil {
+		t.Fatalf("Error reading: %v", err)
+	}
+
+	assert.Equal(t, data, readData, "Data must be equal")
+}
+
+func cleanDirTmp(t *testing.T) {
+	err := os.RemoveAll("tmp")
+	assert.NoError(t, err)
+
+	err = os.Mkdir("tmp", 0755)
+	assert.NoError(t, err)
 }
