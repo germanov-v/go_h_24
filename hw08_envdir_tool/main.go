0ab5b325d371f3dbac31c93f6cbd96f38157d557
valentin <germanov.ve@gmail.com>
2025-02-17 22:01:44 +0300

    HW8 draft

--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -6,7 +6,10 @@ import (
 )
 
 func main() {
-
+	//env, _ := ReadDir("./testdata/env")
+	//code := RunCmd([]string{"/bin/bash", "./testdata/echo.sh", "arg=1", "arg2=2"}, env)
+	//
+	//os.Exit(code)
 	//fmt.Fprintln(os.Stderr, "arguments was not set")
 	//fmt.Fprintln(os.Stdout, "arguments was not set")
 	//fmt.Println(strings.SplitN("dat=val=test", "=", 3))
@@ -22,5 +25,6 @@ func main() {
 	}
 
 	code := RunCmd(cmdArgs, env)
+
 	os.Exit(code)
 }
