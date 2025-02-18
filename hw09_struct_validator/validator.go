112ba2b01a70c93e9a0db4a5a08416225471daff
valentin <germanov.ve@gmail.com>
2025-02-18 10:57:42 +0300

    HW9 draft

--- hw09_struct_validator/validator.go
+++ hw09_struct_validator/validator.go
@@ -4,6 +4,8 @@ import (
 	"errors"
 	"fmt"
 	"reflect"
+	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -44,7 +46,7 @@ type Validator[T any] interface {
 	Validate(T) error
 }
 
-type funValidator[T any] func(T) error
+type funValidator[T any] func(T) error //result,error
 
 func (f funValidator[T]) Validate(v T) error {
 	return f(v)
@@ -53,8 +55,97 @@ func (f funValidator[T]) Validate(v T) error {
 func lenValidator(data int) Validator[string] {
 	return funValidator[string](func(s string) error {
 		if len(s) != data {
+			//return errors.New("len fail")
 			return fmt.Errorf("length of %s is %d, want %d", s, len(s), data)
 		}
 		return nil
 	})
+}
+
+func regValidator(pat string) Validator[string] {
+	r, err := regexp.Compile(pat)
+	if err != nil {
+		return funValidator[string](func(s string) error {
+			return fmt.Errorf("invalid regular expression: %s - %w", s, err)
+		})
+	}
+	return funValidator[string](func(s string) error {
+
+		if !r.MatchString(s) {
+			// с кавычками пробуем формат %q
+			return fmt.Errorf("invalid regular expression: %q  - %w - %q", s, err, pat)
+		}
+
+		return nil
+	})
+}
+
+func containStrValidator(values []string) Validator[string] {
+	return funValidator[string](func(s string) error {
+		for _, v := range values {
+			if v == s {
+				return nil
+			}
+		}
+		return fmt.Errorf("%q not in %v", s, values)
+	})
+}
+
+func minValidator(min int) Validator[int] {
+	return funValidator[int](func(v int) error {
+		if v < min {
+			return fmt.Errorf("value %d is less than minimum %d", v, min)
+		}
+		return nil
+	})
+}
+
+func maxValidator(max int) Validator[int] {
+	return funValidator[int](func(v int) error {
+		if v > max {
+			return fmt.Errorf("value %d is greater than maximum %d", v, max)
+		}
+		return nil
+	})
+}
+
+func containInValidator(values []int) Validator[int] {
+	return funValidator[int](func(val int) error {
+		for _, v := range values {
+			if v == val {
+				return nil
+			}
+		}
+		return fmt.Errorf("value %d not in %v", val, values)
+	})
+}
+
+func createStrRuleValidators(tag string) ([]Validator[string], error) {
+	var validators []Validator[string]
+	rules := strings.Split(tag, "|")
+	for index, rule := range rules {
+		rule = strings.TrimSpace(rule)
+		if rule==""{
+			continue
+		}
+		parts:=strings.SplitN(rule, ":",2)
+		if len(parts) != 2 {
+			return nil, fmt.Errorf("invalid tag rule: %q", rule)
+		}
+
+		switch parts[0] {
+		case "len":
+          expected, err := strconv.Atoi(parts[1])
+		case "regexp":
+
+		case "in"
+		}
+	}
+	return validators, nil
+}
+
+func createIntRuleValidators(tag string) ([]Validator[int], error) {
+	var validators []Validator[int]
+
+	return validators, nil
 }
