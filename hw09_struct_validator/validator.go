7d7f95d6f3d581af4bfdfe6bd8b8c7771ec26679
valentin <germanov.ve@gmail.com>
2025-02-24 22:37:14 +0300

    HW9 draft

--- hw09_struct_validator/validator.go
+++ hw09_struct_validator/validator.go
@@ -9,12 +9,12 @@ import (
 	"strings"
 )
 
+type ValidationErrors []ValidationError
+
 type ValidationError struct {
 	Field string
 	Err   error
 }
-
-type ValidationErrors []ValidationError
 
 func (v ValidationErrors) Error() string {
 	//panic("implement me")
