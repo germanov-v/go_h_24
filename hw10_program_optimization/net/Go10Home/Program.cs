e23d62ff7dbd3a989ebcb0c99ae86a7095d2e06c
Valentin <55596166+germanov-v@users.noreply.github.com>
2025-03-19 06:53:40 +0300

    hw10_program_optimization (#11)
    * HW10 draft
    
    * HW10 draft
    
    * HW10 draft
    
    * HW10 draft
    
    * HW10 draft
    
    * HW10 draft
    
    * HW10 draft
    
    * HW10 draft

---
+++ hw10_program_optimization/net/Go10Home/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Diagnostics;
+
+namespace StreamingJsonFile;
+
+
+
+class Program
+{
+    
+    private const long Mb = 1<<20; // 1 MB 
+    private const long MemoryLimit = 30 * Mb;
+    private static readonly TimeSpan TimeLimit = TimeSpan.FromMicroseconds(300);
+    private static readonly long TimeLimitMilliseconds = 300*1_000;
+    
+    static async Task Main(string[] args)
+    {
+        GC.Collect();       // on demand
+        GC.WaitForPendingFinalizers(); // wait
+        GC.Collect();       //on demand once more time)
+
+        long memoryBefore = GC.GetTotalMemory(true);
+        var stopwatch = new Stopwatch();
+        var parser = new JsonFileParse();
+        var src = new CancellationTokenSource();
+        stopwatch.Start();
+        
+        // await parser.CalculateStat("users.dat", src.Token, false).ConfigureAwait(false);
+        await parser.CalculateStat("users.dat.zip", src.Token, true, true).ConfigureAwait(false);
+
+        stopwatch.Stop();
+        
+        GC.Collect();       // on demand
+        GC.WaitForPendingFinalizers(); // wait
+        GC.Collect();       //on demand once more time)
+        long memoryAfter = GC.GetTotalMemory(true);
+        
+        var factTime = stopwatch.ElapsedMilliseconds * 1000;
+        
+        var deltaMemory = (memoryAfter - memoryBefore);
+        if (deltaMemory <= MemoryLimit)
+        {
+            Console.WriteLine($"Success!!! Memory:  {deltaMemory} mb");
+        }
+        else
+        {
+            Console.WriteLine($"Fail!!! Memory:  {deltaMemory} mb");
+        }
+        
+        if (factTime < TimeLimitMilliseconds)
+        {
+            Console.WriteLine($"Success!!! Elapsed time: {factTime} ms");
+        }
+        else
+        {
+            Console.WriteLine($"Fail!!! Elapsed time: {factTime} ms");
+        }
+        
+        // Console.WriteLine(factTime);
+        // Console.WriteLine(factTime.TotalMicroseconds);
+        // Console.WriteLine(TimeLimit);
+        
+    }
+    
+    
+    
+}
--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -1,10 +1,9 @@
 package hw10programoptimization
 
 import (
-	"encoding/json"
-	"fmt"
+	"bufio"
+	"bytes"
 	"io"
-	"regexp"
 	"strings"
 )
 
@@ -20,47 +19,155 @@ type User struct {
 
 type DomainStat map[string]int
 
+//type users [100_000]User // Array
+
+//var (
+//	emailRegex = regexp.MustCompile(`(?i)\.[a-z]{2,4}$`)
+//	//regexp.MustCompile(`^[a-z0-9._%+\-]+@[A-Za-z0-9.\-]+\.[a-z]{2,4}$`)
+//)
+
+const emailPatternJson = "\"Email\""
+
+const emailStartPatternJson = "@"
+const stopPatternJson = "\""
+
 func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
-	u, err := getUsers(r)
-	if err != nil {
-		return nil, fmt.Errorf("get users error: %w", err)
-	}
-	return countDomains(u, domain)
-}
 
-type users [100_000]User
+	stats := make(DomainStat)
 
-func getUsers(r io.Reader) (result users, err error) {
-	content, err := io.ReadAll(r)
-	if err != nil {
-		return
-	}
-
-	lines := strings.Split(string(content), "\n")
-	for i, line := range lines {
-		var user User
-		if err = json.Unmarshal([]byte(line), &user); err != nil {
-			return
-		}
-		result[i] = user
-	}
-	return
-}
-
-func countDomains(u users, domain string) (DomainStat, error) {
-	result := make(DomainStat)
-
-	for _, user := range u {
-		matched, err := regexp.Match("\\."+domain, []byte(user.Email))
+	fScanner := bufio.NewScanner(r)
+	fScanner.Split(bufio.ScanLines)
+	//domainRegex := //regexp.MustCompile("(?i)^(?:[^@]+@)(.+\\." + regexp.QuoteMeta(domain) + ")$")
+	//regexp.MustCompile("(?i)@(.+?)\\." + regexp.QuoteMeta(domain) + "$")
+	//regexp.MustCompile("(?i)@(.+\\." + regexp.QuoteMeta(domain) + ")$") // !!!
+	//regexp.MustCompile("\\." + regexp.QuoteMeta(domain))
+	//	regexp.MustCompile(`(?i)"Email"\s*:\s*"[^@]+@(.+\.` + regexp.QuoteMeta(domain) + `)"`)
+	for fScanner.Scan() {
+		err := calcDomainsNonJsonNonRegex(fScanner.Bytes(), &stats, domain)
 		if err != nil {
 			return nil, err
 		}
-
-		if matched {
-			num := result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])]
-			num++
-			result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])] = num
-		}
+		//var user User
+		//
+		//err := json.Unmarshal(fScanner.Bytes(), &user)
+		//if err != nil {
+		//	continue
+		//}
+		//err = calcDomains(user, &stats, domainRegex)
+		//if err != nil {
+		//	//	return nil, fmt.Errorf("failed calc domain", err)
+		//	return nil, err
+		//}
 	}
-	return result, nil
+	return stats, nil
 }
+
+func calcDomainsNonJsonNonRegex(b []byte, stats *DomainStat, domain string) error {
+
+	startIdx := bytes.Index(b, []byte(emailPatternJson))
+	if startIdx == -1 {
+		return nil
+	}
+
+	startByte := b[startIdx+len(emailPatternJson):]
+
+	startIdx = bytes.Index(startByte, []byte(emailStartPatternJson))
+
+	if startIdx == -1 {
+		//return errors.New("invalid email address: lost @")
+		return nil
+	}
+
+	startByte = startByte[startIdx+1:]
+
+	startIdx = bytes.Index(startByte, []byte(stopPatternJson))
+
+	if startIdx == -1 {
+		//return errors.New("invalid email address: lost @")
+		return nil
+	}
+
+	startByte = startByte[:startIdx]
+
+	domainIdx := bytes.Index(startByte, []byte(domain))
+
+	if domainIdx == -1 {
+		//	return fmt.Errorf("invalid email address: lost %s", domain)
+		return nil
+	}
+
+	data := startByte[:domainIdx+len(domain)]
+
+	dataStr := string(data)
+	(*stats)[(strings.ToLower(string(dataStr)))]++
+
+	return nil
+}
+
+//
+//func calcDomainsNonJson(b []byte, stats *DomainStat, regexp *regexp.Regexp) error {
+//
+//	matches := regexp.FindSubmatch(b)
+//	if len(matches) <= 1 {
+//		return nil
+//	}
+//
+//	//match := regexp.Match(b)
+//	//if !match {
+//	//	return nil
+//	//}
+//
+//	//_, domain, found := strings.Cut(u.Email, "@")
+//	//if !found {
+//	//	return fmt.Errorf("invalid email address")
+//	//}
+//	(*stats)[(strings.ToLower(string(matches[1])))]++
+//	//(*stats)[(domain)]++
+//	//(*stats)[customToLowe(matches[1])]++
+//	return nil
+//}
+
+//
+//func calcDomains(u User, stats *DomainStat, regexp *regexp.Regexp) error {
+//
+//	//matches := regexp.FindStringSubmatch(u.Email)
+//	//if len(matches) <= 1 {
+//	//	return nil
+//	//}
+//
+//	match := regexp.MatchString(u.Email)
+//	if !match {
+//		return nil
+//	}
+//
+//	_, domain, found := strings.Cut(u.Email, "@")
+//	if !found {
+//		return fmt.Errorf("invalid email address")
+//	}
+//	(*stats)[(strings.ToLower(domain))]++
+//	//(*stats)[(domain)]++
+//	//(*stats)[customToLowe(matches[1])]++
+//	return nil
+//}
+
+//func customToLower(s string) string {
+//	for i := 0; i < len(s); i++ {
+//		c := s[i]
+//		//
+//		if c >= 'A' && c <= 'Z' {
+//
+//			b := make([]byte, len(s))
+//			copy(b, s[:i]) // здесь уже в нижнем регистре
+//			for j := i; j < len(s); j++ {
+//				c = s[j]
+//				if c >= 'A' && c <= 'Z' {
+//					b[j] = c + 32 // для ASCII сдвигаемся: A-65 => a-97
+//				} else {
+//					b[j] = c
+//				}
+//			}
+//			return string(b)
+//		}
+//	}
+//	return s
+//}
